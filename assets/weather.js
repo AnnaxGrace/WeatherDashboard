d05b87ef490e14be9e2213fbca2b1c394aa815ab
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-04 09:42:48 -0500

    Made search button clickable

---
+++ assets/weather.js
@@ -0,0 +1,27 @@
+
+// var city = $(".inputCity").val()
+
+
+$(".searchBtn").on("click", function(event) {
+
+    event.preventDefault();
+    var city = $(".inputCity").val()
+    console.log(city);
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // })
+    //     .then(function(response) {
+        
+    //         console.log(response);
+        
+    // });
+
+});
+
+
+
+
+
+
