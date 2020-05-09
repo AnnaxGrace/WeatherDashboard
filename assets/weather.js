7b3933ffc622552cff748929fbc7fb2be970cf25
AnnaGrace <anna.grace.conover@gmail.com>
2020-05-09 11:02:05 -0500

    Created all of the current api text

--- assets/weather.js
+++ assets/weather.js
@@ -6,10 +6,12 @@ $(".searchBtn").on("click", function(event) {
 
     event.preventDefault();
     var city = $(".inputCity").val()
-    console.log(city);
+    // console.log(city);
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=84c9f7b047fa7da5be9f1cbf36387401"
-
+    var currentURL="https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=84c9f7b047fa7da5be9f1cbf36387401"
+    // var uvURL = "api.openweathermap.org/data/2.5/uvi/forecast?lat=" + 37.75 "&lon=" + -122.37 + "&appid=84c9f7b047fa7da5be9f1cbf36387401"
+    //need to get lat and long from city to do uv
 
     $.ajax({
         url: queryURL,
@@ -24,6 +26,7 @@ $(".searchBtn").on("click", function(event) {
             var tempF3 = (response.list[20].main.temp - 273.15) * 1.80 + 32;
             var tempF4 = (response.list[28].main.temp - 273.15) * 1.80 + 32;
             var tempF5 = (response.list[36].main.temp - 273.15) * 1.80 + 32;
+        
 
             $(".Date1").text(response.list[4].dt_txt);
             $(".Date2").text(response.list[12].dt_txt);
@@ -48,42 +51,72 @@ $(".searchBtn").on("click", function(event) {
             var newIcon1URL = "http://openweathermap.org/img/w/" + newIcon1 + ".png";
             var newImage1 = $("<img>")
             newImage1.attr("src", newIcon1URL);
+            $("#icon1").empty();
             $("#icon1").append(newImage1)
 
             var newIcon2 = response.list[12].weather[0].icon
             var newIcon2URL = "http://openweathermap.org/img/w/" + newIcon2 + ".png";
             var newImage2 = $("<img>")
             newImage2.attr("src", newIcon2URL);
+            $("#icon2").empty();
             $("#icon2").append(newImage2)
 
             var newIcon3 = response.list[20].weather[0].icon
             var newIcon3URL = "http://openweathermap.org/img/w/" + newIcon3 + ".png";
             var newImage3 = $("<img>")
             newImage3.attr("src", newIcon3URL);
+            $("#icon3").empty();
             $("#icon3").append(newImage3)
 
             var newIcon4 = response.list[28].weather[0].icon
             var newIcon4URL = "http://openweathermap.org/img/w/" + newIcon4 + ".png";
             var newImage4 = $("<img>")
             newImage4.attr("src", newIcon4URL);
+            $("#icon4").empty();
             $("#icon4").append(newImage4)
 
             var newIcon5 = response.list[36].weather[0].icon
             var newIcon5URL = "http://openweathermap.org/img/w/" + newIcon5 + ".png";
             var newImage5 = $("<img>")
             newImage5.attr("src", newIcon5URL);
+            $("#icon5").empty();
             $("#icon5").append(newImage5)
 
-            // $(".headCity").append(response.city);
-            // $(".currentCity").append("Temperature: " + response.city);
-            // $(".currentCity").append("Humidity: " + response.city);
-            // $(".currentCity").append("Wind Speed: " + response.city);
-            // $(".currentCity").append("UV Index: " + response.uv);
-
+    
 
 
         
     });
+
+    $.ajax ({
+        url: currentURL,
+        method: "GET"
+    })
+    .then(function(result) {
+
+        console.log(result);
+
+        var tempCurrent = (result.main.temp - 273.15) * 1.80 + 32;
+
+        $(".headCity").text(result.name);
+        console.log(result.name + result.dt);
+        $(".currentCityTemp").text("Temperature: " + tempCurrent.toFixed(2));
+        $(".currentCityHumidity").text("Humidity: " + result.main.humidity);
+        $(".currentCityWind").text("Wind Speed: " + result.wind.speed);
+       
+
+
+
+    });
+
+    // $.ajax ({
+    //     url: uvURL,
+    //     method: "GET"
+    // })
+    // .then(function(uvresponse) {
+        // $(".currentCity").append("UV Index: " + result.list[0].uv);
+    
+    // });
 
 });
 
